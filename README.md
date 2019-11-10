1e7f02b9a850cf91a8691bf6f0993c044ea804af
luiscastro193 <luiscastro193@gmail.com>
2019-11-10 10:34:53 +0100

    Lazy loading

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # waitForGlobal
 Extremely simple yet effective waitForGlobal function for browsers
-
-Reminder: dinamically adding a script tag to head works
---
+++ main.js
@@ -0,0 +1,31 @@
+function waitForGlobal(name) {
+	return new Promise(resolve => {
+		if (window[name])
+			return resolve();
+		
+		document.head.querySelector(`[meta-id=${name}]`).addEventListener('load', () => resolve());
+	});
+}
+
+function appendScriptTag(url, id) {
+	let script = document.createElement( 'script' );
+	script.setAttribute('src', url);
+	script.setAttribute('meta-id', id);
+	script.setAttribute('async', '');
+	document.head.appendChild(script);
+}
+
+function loadLibrary(url, global) {
+	if (!document.head.querySelector(`[meta-id=${global}]`))
+		appendScriptTag(url, global);
+	
+	return waitForGlobal(global);
+}
+
+async function test() {
+	await loadLibrary("https://cdn.plot.ly/plotly-latest.min.js", "Plotly");
+	console.log(Plotly);
+	document.querySelector('p').textContent = "Library loaded";
+}
+
+test();
